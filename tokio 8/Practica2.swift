d2fc5b23699332fb892739441af46f50fb0945b5
Osasu Sanachez <osasusan@gmail.com>
2026-04-08 18:01:10 +0200

    uardado de datos en UserDefaults y ficheros XML

--- tokio 8/Practica2.swift
+++ tokio 8/Practica2.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class Practica2: UIViewController {
+class Practica2: UIViewController,UITextFieldDelegate {
     
     @IBOutlet weak var tfUsername: UITextField!
     @IBOutlet weak var tfPass: UITextField!
@@ -20,7 +20,7 @@ class Practica2: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-            // Asignamos el delegate a los dos campos
+            
         tfUsername.delegate = self
         tfPass.delegate = self
         
@@ -41,36 +41,18 @@ class Practica2: UIViewController {
             tvMostrar.text = "No hay datos guardados"
         }
     }
-}
-
-    // MARK: - UITextFieldDelegate
-extension Practica2: UITextFieldDelegate {
-    
-        //ocultarr con return
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-       //guardarr auto
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        guardarSiHayDatos()
-    }
-}
-
-    // MARK: - Lógica de guardado
-extension Practica2 {
-    
     func guardarSiHayDatos() {
         guard let user = tfUsername.text, !user.isEmpty,
               let pass = tfPass.text, !pass.isEmpty else {
             tvMostrar.text = "Rellena usuario y contraseña"
             return
         }
+        userDefaults.removeObject(forKey: keyUser2)
         saveUser(user: user, pass: pass)
     }
     
     func saveUser(user: String, pass: String) {
-        userDefaults.removeObject(forKey: keyUser2)
+        
         do {
             let userToSave = User(user: user, pass: pass)
             let data = try JSONEncoder().encode(userToSave)
@@ -81,6 +63,22 @@ extension Practica2 {
             print("Error al codificar: \(error)")
         }
     }
+        //ocultarr con return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+        //guardarr auto
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guardarSiHayDatos()
+    }
 }
 
+    
+
+
+   
+
+    
+    
 
